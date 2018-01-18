b8069bdfe86418650197c6dfd6cb72714f230de2
Sean Hickey <hisean@media.mit.edu>
2018-01-18 18:18:08 -0500

    Integrating firebase and messing around with zoom

--- TODO.md
+++ TODO.md
@@ -1,14 +1,14 @@
 # TODO
 
-- App boot
-- Create nodes
-- Link nodes
-- Data storage/backend
-- Differentiate nodes (text, image, video, URL, etc)
-- Users and authorship
-- Visibility rules (public by default, private by choice)
-- Collaborative spaces
-- Activity log
-- Note deletion (with ghosting)
-- Custom views
-- Commenting? (Does this actually add value? Should comments just be their own nodes?)
+[x] App boot
+[ ] Create nodes
+[ ] Link nodes
+[ ] Data storage/backend
+[ ] Differentiate nodes (text, image, video, URL, etc)
+[ ] Users and authorship
+[ ] Visibility rules (public by default, private by choice)
+[ ] Collaborative spaces
+[ ] Activity log
+[ ] Note deletion (with ghosting)
+[ ] Custom views
+[ ] Commenting? (Does this actually add value? Should comments just be their own nodes?)
--- rhizome.js
+++ rhizome.js
@@ -10,23 +10,73 @@
  */
 
 ;(function() {
+
+  'use strict';
+
+  // Initialize Firebase
+  var firebaseConfig = {
+    apiKey: "AIzaSyBaWc2sIScNik2lrUdr4DQOz1tyC_F48Ww",
+    authDomain: "rhizome-18e8b.firebaseapp.com",
+    databaseURL: "https://rhizome-18e8b.firebaseio.com",
+    projectId: "rhizome-18e8b",
+    storageBucket: "rhizome-18e8b.appspot.com",
+    messagingSenderId: "614011893394"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  var initialZoom = d3.zoomIdentity.translate((-document.body.clientWidth / 2), (-document.body.clientHeight / 2)).scale(1);
+  var zoom = d3.zoom().on('zoom', function() {
+    d3.select('svg').attr('transform', d3.event.transform);
+  });
   var svg = d3.select('body')
     .append('svg')
     .attr('width', '100%')
     .attr('height', '100%')
-    .call(d3.zoom().on('zoom', function() {
-      svg.attr('transform', d3.event.transform);
-    }))
-    .append('g');
+    .call(zoom)
+    .call(zoom.transform, initialZoom)
+    .append('g')
+      .attr('transform', initialZoom);
 
-  var data = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+
+  var data = [
+    {
+      x: 0,
+      y: 0,
+      color: "red"
+    },
+    {
+      x: 100,
+      y: 0,
+      color: "orange"
+    },
+    {
+      x: 150,
+      y: 100,
+      color: "yellow"
+    },
+    {
+      x: -100,
+      y: 100,
+      color: "green"
+    },
+    {
+      x: 300,
+      y: 50,
+      color: "blue"
+    },
+    {
+      x: 100,
+      y: -150,
+      color: "purple"
+    },
+  ];
 
   svg.selectAll('circle')
     .data(data)
     .enter()
       .append('circle')
-      .attr("cx", function(d, i) { return (document.body.clientWidth / 2) + (200 * i); })
-      .attr("cy", document.body.clientHeight / 2)
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; })
       .attr("r", 50)
-      .style("fill", "#aaa");
+      .style("fill", function(d) { return d.color; });
 })();
